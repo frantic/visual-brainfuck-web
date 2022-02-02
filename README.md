53746d578ac6ff2ff1d5e649d69f2a4cf11c9cda
ZZHZZHZZH <zhangzheheng12345@163.com>
2022-02-02 15:31:08 +0800

    Add visual run feature

--- README.md
+++ README.md
@@ -5,10 +5,9 @@ If you don't have any idea about what Brainf\*ck exactly is, visit [Brainf\*ck w
 
 ### How to use
 
-The Run button runs the Brainf\*ck code in the large text area. The Show Data button shows or hides the data. The input widget after IO: enables you to type your inputs ( "," command in Brainf\*ck will get the input from here, but you should get the inputs ready before press the Run button), and see the outputs ( "." command in Brainf\*ck will output to here).
+The Run button runs the Brainf\*ck code in the large text area. The Show Data button shows or hides the data. The data area will show
+you how the program change the pointer's position and change the data. ( The black block is where the pointer located.
+
+The input widget after IO: enables you to type your inputs ( "," command in Brainf\*ck will get the input from here, but you should get the inputs ready before press the Run button), and see the outputs ( "." command in Brainf\*ck will output to here).
 
 This interpreter also provides some features which the Brainf**k standard doesn't include. For example, '#' declares a comment which continues to the end of the line. And the maximum number of a memory cell is not 255, but the maximum value of the ``` number ``` type in Javascript.
-
-### Will support
-
-A visual runner, which shows how the pointer moves and how the BF program changes data.
--- main.js
+++ main.js
@@ -1,50 +1,6 @@
 $("#data").hide()
 
-$("#run").click(function() {
-    let dindex = 0
-    let cindex = 0
-    let iindex = 0
-    let code = $("#text").val()
-    let input = $("#io").val()
-    let output = ""
-    let data = [0]
-    let stack = []
-    while(cindex < code.length) {
-        if(code[cindex] == "+") {
-            data[dindex] += 1
-        } else if(code[cindex] == "-") {
-            data[dindex] -= 1
-        } else if(code[cindex] == ">") {
-            dindex += 1
-            if(dindex >= data.length) {
-                data.push(0)
-            }
-        } else if(code[cindex] == "<") {
-            if(dindex > 0) {
-                dindex -= 1
-            }
-        } else if(code[cindex] == "[") {
-            stack.push(cindex)
-        } else if(code[cindex] == "]") {
-            if(data[dindex])
-                cindex = stack[stack.length - 1];
-            else
-                stack.pop();
-        } else if(code[cindex] == ",") {
-            data[dindex] = input[iindex].charCodeAt()
-            iindex += 1
-        } else if(code[cindex] == ".") {
-            output += String.fromCharCode(data[dindex])
-        } else if(code[cindex] == "#") {
-            while(cindex < code.length && code[cindex] != "\n") {
-                cindex += 1
-            }
-        }
-        cindex += 1
-    }
-    $("#data").text(joinData(data))
-    $("#io").val(output)
-})
+$("#run").click(runCode)
 
 $("#showData").click(function() {
     $("#data").slideToggle()
@@ -54,9 +10,67 @@ $("#showData").click(function() {
         $(this).text("Show Data");
 })
 
-function joinData(data) {
-    if(data.length > 0)
-        return new String(data[0]) + " " + joinData(data.slice(1, data.length));
-    else
-        return "";
+$("#clearCode").click(function() {
+    $("#text").val("")
+})
+
+function runCode() {
+    let dindex = 0
+    let cindex = 0
+    let iindex = 0
+    let code = $("#text").val()
+    let input = $("#io").val()
+    $("#io").val("")
+    let data = [0]
+    let stack = []
+    $("#data").empty()
+    $("#data").append($("<div id=0></div>").text(0))
+    $("#0").hide().slideDown()
+    function iterLoop() {
+        if(cindex < code.length) {
+            if(code[cindex] == "+") {
+                data[dindex] += 1
+                $("#" + dindex).text(new String(data[dindex]))
+            } else if(code[cindex] == "-") {
+                data[dindex] -= 1
+                $("#" + dindex).text(new String(data[dindex]))
+            } else if(code[cindex] == ">") {
+                dindex += 1
+                if(dindex >= data.length) {
+                    data.push(0)
+                    $("#data").append($("<div id="+data.length+"></div>").text("0"))
+                    $("#"+data.length).hide().slideDown()
+                }
+                updatePtr(dindex-1)
+            } else if(code[cindex] == "<") {
+                if(dindex > 0) {
+                    dindex -= 1
+                }
+                updatePtr(dindex+1)
+            } else if(code[cindex] == "[") {
+                stack.push(cindex)
+            } else if(code[cindex] == "]") {
+                if(data[dindex])
+                    cindex = stack[stack.length - 1];
+                else
+                    stack.pop();
+            } else if(code[cindex] == ",") {
+                data[dindex] = input[iindex].charCodeAt()
+                iindex += 1
+            } else if(code[cindex] == ".") {
+                $("#io").val($("#io").val() + String.fromCharCode(data[dindex]))
+            } else if(code[cindex] == "#") {
+                while(cindex < code.length && code[cindex] != "\n") {
+                    cindex += 1
+                }
+            }
+            cindex += 1
+            setTimeout(iterLoop, 50)
+        }
+    }
+    function updatePtr(origin) {
+        $("#"+origin).css("background-color", "#555")
+        $("#"+dindex).css("background-color", "#222")
+    }
+    iterLoop()
 }
