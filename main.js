0193add5f71106cded1652bc8bcc0bfe056a9f00
ZZH <76390451+zhangzheheng12345@users.noreply.github.com>
2022-02-10 14:44:53 +0800

    Default the data area is open now, if it's hidden, program will immediately run but not updating the data area

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 window.toRun = true
 $("#stop").css("background-color", "#999")
 
-$("#data").hide()
 $("#run").click(runCode)
 $("#stop").click(() => { window.toRun = false; })
 $("#showData").click(() => {
@@ -30,6 +29,46 @@ function runCode() {
     $("#data").empty()
     $("#data").append($("<div id='0'></div>").text(0))
     updatePtr(0)
+    function immRun() {
+        while(cindex < code.length && window.toRun) {
+            if(code[cindex] == "+") {
+                data[dindex] += 1
+            } else if(code[cindex] == "-") {
+                data[dindex] -= 1
+            } else if(code[cindex] == ">") {
+                dindex += 1
+                if(dindex >= data.length) {
+                    data.push(0)
+                }
+            } else if(code[cindex] == "<") {
+                if(dindex > 0) {
+                    dindex -= 1
+                }
+            } else if(code[cindex] == "[") {
+                stack.push(cindex)
+            } else if(code[cindex] == "]") {
+                if(data[dindex])
+                    cindex = stack[stack.length - 1];
+                else
+                    stack.pop();
+            } else if(code[cindex] == ",") {
+                if(iindex < input.length)
+                    data[dindex] = input[iindex].charCodeAt();
+                else
+                    data[dindex] = 0;
+                iindex += 1
+            } else if(code[cindex] == ".") {
+                $("#io").val($("#io").val() + String.fromCharCode(data[dindex]))
+            } else if(code[cindex] == "#") {
+                while(cindex < code.length && code[cindex] != "\n") {
+                    cindex += 1
+                }
+            }
+            cindex += 1
+        }
+        $("#run").css("background-color", "#666")
+        $("#stop").css("background-color", "#aaa")
+    }
     function iterLoop() {
         if(cindex < code.length) {
             if(code[cindex] == "+") {
@@ -87,5 +126,9 @@ function runCode() {
         $("#"+origin).css({"background-color":"#555", "transform":"scale(1.0, 1.0)"})
         $("#"+dindex).css({"background-color":"#222", "transform":"scale(1.1, 1.1)"})
     }
-    setTimeout(iterLoop, 50)
+    if($("#showData").text() == "Show Data"){
+        immRun()
+    } else {
+        setTimeout(iterLoop, 50)
+    }
 }
